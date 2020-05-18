8bbfbbfe028917a1974bb1edefd8e0381d0000ff
Gabriel Oliveira <biel--oliveira@hotmail.com>
2020-05-18 17:50:12 -0300

    Componente FormRow

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+
+import FormRow from '../components/FormRow'
 
 export default class LoginPage extends React.Component {
     render(){
         return (
             <View>
-                <Text>Login!!</Text>
+                <FormRow>
+                    <TextInput></TextInput>
+                </FormRow>
             </View>
         )
     }
