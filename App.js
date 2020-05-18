5a576b87d7bc130140bd99a0038264ec0d203160
Gabriel Oliveira <biel--oliveira@hotmail.com>
2020-05-18 17:21:41 -0300

    Configuração Basica do Header

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import {createStackNavigator, HeaderTitle} from 'react-navigation-stack';
 
 import LoginScreen from './src/pages/LoginScreen'
 
 const AppNavigator = createStackNavigator({
   'Login':{
-    screen: LoginScreen
+    screen: LoginScreen,
+    navigationOptions: {
+      title: 'Bem Vindo!'
+    }
   },
+}, {
+  defaultNavigationOptions: {
+    title: "Series",
+    headerTintColor: 'white',
+    headerStyle: {
+      backgroundColor: '#6ca2f7',
+      borderBottomWidth: 1,
+      borderBottomColor: '#C5C5C5',
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontSize: 30,
+    }
+  }
 });
 
 const AppContainer = createAppContainer(AppNavigator);
